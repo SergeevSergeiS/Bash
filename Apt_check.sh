8f9ddbb7a61d63c7551ce9ab0ab574c12b0d84f5
Sergei Sergeev <68876374+SergeevSergeiS@users.noreply.github.com>
2021-08-31 16:48:27 +0400

    Update Apt_check.sh

--- Apt_check.sh
+++ Apt_check.sh
@@ -1,1 +1,11 @@
+#! /bin/bash
 
+while [[ `ps aux | grep -i apt | wc -l` != 1 ]] ; do
+    echo 'apt is locked by another process.'
+    sleep 15
+    ps aux | grep -i apt | wc -l
+done
+
+echo 'apt is free. Let`s continue.'
+
+echo $'\n'
